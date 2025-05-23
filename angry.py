72fb7d9f99fbc637a8b97d94cfee778b7ef0c439
Alex Chandler <chandlia30588@hotmail.com>
2025-05-23 20:26:50 +0800

    completed portfolio

---
+++ angry.py
@@ -0,0 +1,31 @@
+import time
+from blinkable import Blinkable
+from smiley import Smiley
+
+class Angry(Smiley, Blinkable):
+    def __init__(self):
+        super().__init__(complexion=self.RED)
+
+        self.draw_mouth()
+        self.draw_eyes()
+
+    def draw_mouth(self):
+        mouth = [41, 42, 43, 44, 45, 46]
+        for pixel in mouth:
+            self.pixels[pixel] = self.BLANK
+
+    def draw_eyes(self, wide_open=True):
+        eyes = [10, 13, 18, 21]
+        for pixel in eyes:
+            if wide_open:
+                eyes = self.BLANK
+            else:
+                eyes = self.complexion()
+            self.pixels[pixel] = eyes
+
+    def blink(self, delay = 0.25):
+        self.draw_eyes(wide_open=False)
+        self.show()
+        time.sleep(delay)
+        self.draw_eyes(wide_open=True)
+        self.show()
